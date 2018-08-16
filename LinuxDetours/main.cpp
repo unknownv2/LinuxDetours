558061c91d9eec44f844a97ecb0eda0b86daf0ba
unknown v2 <unknownv2@users.noreply.github.com>
2018-08-16 18:00:00 -0400

    Update main test code

--- LinuxDetours/main.cpp
+++ LinuxDetours/main.cpp
@@ -6,7 +6,7 @@ static unsigned int(WINAPI * TrueSleepEx)(unsigned int seconds) = sleep;
 
 unsigned int WINAPI TimedSleepEx(unsigned int seconds)
 {
-	DWORD ret = TrueSleepEx(seconds);
+	DWORD ret = sleep(seconds);
 
 	return ret;
 }
@@ -14,7 +14,7 @@ unsigned int WINAPI TestDetourB(unsigned int seconds, unsigned int a, unsigned i
 {
 	return seconds + 1;
 }
-unsigned int WINAPI TestDetourA(unsigned int seconds, unsigned int a, unsigned int b, unsigned int c, unsigned int d, unsigned int e)
+unsigned int WINAPI TestDetourA(unsigned int seconds, unsigned int a, unsigned int b, unsigned int c, unsigned int d, unsigned int e, unsigned int f, unsigned int g, unsigned int h)
 {
 	printf("Detoured B -> A %d %d %d %d %d %d\n", a, b, c, d, e);
 	return TestDetourB(seconds + 2, a, b, c, d, e);
@@ -28,10 +28,10 @@ VOID* WINAPI TestSleep(void*)
 	printf("detours: Calling sleep for 1 second.\n");
 	sleep(1);
 	printf("detours: Calling sleep again for 1 second.\n");
-	sleep(1);
+	sleep(2);
 	
 	printf("detours: Done sleeping.\n\n");
-
+	
 	return NULL;
 }
 float test2(float a1, float a2, float a3)
@@ -52,10 +52,27 @@ double test1d(double a1, double a2, double a3)
 	float a4 = a1 + a2 * a3;
 	return test2d(a4, a2, a1);
 }
+LONG DetDetourUpdateThread(pthread_t threadId)
+{
+	return DetourUpdateThread(threadId);
+}
+#define BITS 32
+
+int rotateRight(int num, int by)
+{
+	return (num >> by) | (num << (BITS - by));
+}
+int rotateLeft(int num, int by)
+{
+	return (num << by) | (num >> (BITS - by));
+}
 int main()
 {
 	//__float128 ss = 128.0;
 	//TestDetourA(2, 3, 4,5,6,7);
+	//int as = rotateRight(0x1b, 0x14);
+	//int ass = rotateLeft(0x1b, 0x14);
+
 	//test1d(1.0, 2.0,3.0);
 	
 	LhBarrierProcessAttach();
@@ -66,15 +83,21 @@ int main()
 	LONG selfHandle2 = 0;
 	TRACED_HOOK_HANDLE outHandle = new HOOK_TRACE_INFO();
 	TRACED_HOOK_HANDLE outHandle2 = new HOOK_TRACE_INFO();
-
+	
 	//sleep(1);
-	//LhInstallHook((void*)TestDetourB, (void*)TestDetourA, &selfHandle, outHandle);
-	LhInstallHook((void*)TrueSleepEx, (void*)TimedSleepEx, &selfHandle2, outHandle2);
+	//LhInstallHook((void*)DetourUpdateThread, (void*)DetDetourUpdateThread, &selfHandle2, outHandle2);
+	
+	LhInstallHook((void*)sleep, (void*)TimedSleepEx, &selfHandle2, outHandle2);
+	
+	LhInstallHook((void*)TestDetourB, (void*)TestDetourA, &selfHandle, outHandle);
 	ULONG ret = LhSetExclusiveACL(new ULONG(), 1, (TRACED_HOOK_HANDLE)outHandle);
 	ret = LhSetExclusiveACL(new ULONG(), 1, (TRACED_HOOK_HANDLE)outHandle2);
+
 	pthread_t t;
 	pthread_create(&t, NULL, TestSleep, NULL);
 	pthread_join(t, NULL);
+
+
 	LhUninstallHook(outHandle);
 	LhUninstallHook(outHandle2);
 
@@ -82,6 +105,9 @@ int main()
 	delete outHandle2;
 
 	sleep(1);
+
+	LhBarrierProcessDetach();
+	LhCriticalFinalize();
 
 	return 0;
 }
