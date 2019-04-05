cd4655dd198f9657aa9b320f2ee2d82a739501ce
Thierry <thierry44@gmail.com>
2019-04-05 01:30:34 -0400

    Remove test code from detours application

--- LinuxDetours/main.cpp
+++ LinuxDetours/main.cpp
@@ -20,92 +20,6 @@ unsigned int TestDetourA(unsigned int seconds, unsigned int a, unsigned int b, u
     return TestDetourB(seconds + 2, a, b, c, d, e);
 }
 
-#if defined(_ARM64_)
-
-long var;
-
-void test_generic_constraints(int var32, long var64) {
-    asm("add %0, %1, %1" : "=r"(var32) : "0"(var32));
-    // CHECK: [[R32_ARG:%[a-zA-Z0-9]+]] = load i32, i32*
-    // CHECK: call i32 asm "add $0, $1, $1", "=r,0"(i32 [[R32_ARG]])
-
-    asm("add %0, %1, %1" : "=r"(var64) : "0"(var64));
-    // CHECK: [[R32_ARG:%[a-zA-Z0-9]+]] = load i64, i64*
-    // CHECK: call i64 asm "add $0, $1, $1", "=r,0"(i64 [[R32_ARG]])
-
-    asm("ldr %0, %1" : "=r"(var32) : "m"(var));
-    asm("ldr %0, [%1]" : "=r"(var64) : "r"(&var));
-    // CHECK: call i32 asm "ldr $0, $1", "=r,*m"(i64* @var)
-    // CHECK: call i64 asm "ldr $0, [$1]", "=r,r"(i64* @var)
-}
-void test_generic_constraints2(int var32, long var64) {
-    asm("add x0, x1, x1");
-    // CHECK: [[R32_ARG:%[a-zA-Z0-9]+]] = load i32, i32*
-    // CHECK: call i32 asm "add $0, $1, $1", "=r,0"(i32 [[R32_ARG]])
-
-    asm("add %0, %1, %1" : "=r"(var64) : "0"(var64));
-    // CHECK: [[R32_ARG:%[a-zA-Z0-9]+]] = load i64, i64*
-    // CHECK: call i64 asm "add $0, $1, $1", "=r,0"(i64 [[R32_ARG]])
-
-    asm("ldr %0, %1" : "=r"(var32) : "m"(var));
-    asm("ldr %0, [%1]" : "=r"(var64) : "r"(&var));
-    // CHECK: call i32 asm "ldr $0, $1", "=r,*m"(i64* @var)
-    // CHECK: call i64 asm "ldr $0, [$1]", "=r,r"(i64* @var)
-}
-
-__attribute__((naked)) long add(); /* Declaring a function with __attribute__((naked)). */
-
-__attribute__((naked)) long add()
-{
-    asm("mov w0, #1;"
-        "ret"); /* Basic assembler statements are supported. */
-}
-#endif
-#if defined(_ARM_)
-
-void test_generic_constraints(int var, int var32, long var64) {
-
-    asm("ldr %0, %1" : "=r"(var32) : "m"(var));
-    asm("ldr %0, [%1]" : "=r"(var64) : "r"(&var));
-    asm("ldr r0, label_1;"
-        "b label_2;"
-        "bl label_2;"
-        "nop;"
-        "label_1:"
-        ".word 0x01020304;"
-        "label_2:"
-        "mov r0, 0;"
-        "mov r1, 1;"
-        "mov r2, 2;"
-        "dmb ish;"
-        "bx lr;"
-    );
-
-    // CHECK: call i32 asm "ldr $0, $1", "=r,*m"(i64* @var)
-    // CHECK: call i64 asm "ldr $0, [$1]", "=r,r"(i64* @var)
-}
-void test_generic_constraints_dest(int var, int var32, long var64) {
-
-    asm("ldr %0, %1" : "=r"(var32) : "m"(var));
-    asm("ldr %0, [%1]" : "=r"(var64) : "r"(&var));
-    asm("ldr r0, label_11;"
-        "b label_21;"
-        "bl label_21;"
-        "nop;"
-        "label_11:"
-        ".word 0x01020304;"
-        "label_21:"
-        "mov r0, 0;"
-        "mov r1, 1;"
-        "mov r2, 2;"
-        "dmb ish;"
-        "bx lr;"
-    );
-
-    // CHECK: call i32 asm "ldr $0, $1", "=r,*m"(i64* @var)
-    // CHECK: call i64 asm "ldr $0, [$1]", "=r,r"(i64* @var)
-}
-#endif
 VOID* TestSleep(void*)
 {
     LOG(INFO) << "detours: TestDetourB returned " << TestDetourB(1, 2, 3, 4, 5, 6);
@@ -118,45 +32,7 @@ VOID* TestSleep(void*)
     
     return NULL;
 }
-float test2(float a1, float a2, float a3)
-{
-    return a1 + a2 * a3;
-}
-float test1(float a1, float a2, float a3)
-{
-    float a4 = a1 + a2 * a3;
-    return test2(a4, a2, a1);
-}
-double test2d(double a1, double a2, double a3)
-{
-    return a1 + a2 * a3;
-}
-double test1d(double a1, double a2, double a3)
-{
-    float a4 = a1 + a2 * a3;
-    return test2d(a4, a2, a1);
-}
-LONG DetDetourUpdateThread(pthread_t threadId)
-{
-    return DetourUpdateThread(threadId);
-}
 
-#define BITS 32
-
-int rotateRight(int num, int by)
-{
-    return (num >> by) | (num << (BITS - by));
-}
-int rotateLeft(int num, int by)
-{
-    return (num << by) | (num >> (BITS - by));
-}
-
-
-/*#include "spdlog/spdlog.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
-namespace spd = spdlog;
-*/
 int test_glog(char * argv)
 {
     google::InitGoogleLogging(argv);
@@ -171,75 +47,35 @@ void* DetourSetSystemRegionLowerBound_detour(void * bound)
 
     return NULL;
 }
-int temp = 4;
-int s_temp[0x20] = { 0 };
 
 int main(int argc, char * argv[])
 {
-    //__float128 ss = 128.0;
-    //TestDetourA(2, 3, 4,5,6,7);
-    //int as = rotateRight(0x1b, 0x14);
-    //int ass = rotateLeft(0x1b, 0x14);
-
-    //test1d(1.0, 2.0,3.0);
-
-    //test_trampoline();
-    //auto console = spd::stdout_color_mt("console");
-    //console->info("Welcome to spdlog!");
-    //console->error("Some error message with arg{}..", 1);
-
     test_glog(argv[0]);
 
-    //test_generic_constraints(s_temp[6], 1, 2);
     DetourBarrierProcessAttach();
 
     DetourCriticalInitialize();
 
-    LONG selfHandle = 0;
-    LONG selfHandle2 = 0;
-    TRACED_HOOK_HANDLE outHandle = new HOOK_TRACE_INFO();
-    TRACED_HOOK_HANDLE outHandle2 = new HOOK_TRACE_INFO();
-    
-    //sleep(1);
-    //DetourInstallHook((void*)DetourUpdateThread, (void*)DetDetourUpdateThread, &selfHandle2, outHandle2);
-    
-#ifdef DETOURS_ARM
-#ifdef DETOURS_ARM32
-    // BL XXX instruction
-    DetourInstallHook((unsigned char*)test_generic_constraints + 0x30,
-        (void*)test_generic_constraints_dest, &selfHandle2, outHandle2);
-    // B XXX instruction
-    DetourInstallHook((unsigned char*)test_generic_constraints + 0x2C,
-        (void*)DetourSetSystemRegionLowerBound_detour, &selfHandle2, outHandle2);
-    // LDR r0, [PC + XXX] instruction
-    DetourInstallHook((unsigned char*)test_generic_constraints + 0x28,
-        (void*)DetourSetSystemRegionLowerBound_detour, &selfHandle2, outHandle2);
-#else
-    // B XXX instruction
-    DetourInstallHook((unsigned char*)test_generic_constraints + 0x20, 
-        (void*)DetourSetSystemRegionLowerBound_detour, &selfHandle2, outHandle2);
-    // LDR r0, [PC + XXX] instruction
-    DetourInstallHook((unsigned char*)test_generic_constraints + 0x1C, 
-        (void*)DetourSetSystemRegionLowerBound_detour, &selfHandle2, outHandle2);
-#endif
-#endif
+    LONG test_detour_callback = 0;
+    LONG sleep_detour_callback = 0;
+    TRACED_HOOK_HANDLE test_detour_handle = new HOOK_TRACE_INFO();
+    TRACED_HOOK_HANDLE sleep_detour_handle = new HOOK_TRACE_INFO();    
 
-    DetourInstallHook((void*)TestDetourB, (void*)TestDetourA, &selfHandle, outHandle);
-    DetourInstallHook((void*)sleep, (void*)sleep_detour, &selfHandle2, outHandle2);
+    DetourInstallHook((void*)TestDetourB, (void*)TestDetourA, &test_detour_callback, test_detour_handle);
+    DetourInstallHook((void*)sleep, (void*)sleep_detour, &sleep_detour_callback, sleep_detour_handle);
 
-    ULONG ret = DetourSetExclusiveACL(new ULONG(), 1, (TRACED_HOOK_HANDLE)outHandle);
-    ret = DetourSetExclusiveACL(new ULONG(), 1, (TRACED_HOOK_HANDLE)outHandle2);
+    ULONG ret = DetourSetExclusiveACL(new ULONG(), 1, (TRACED_HOOK_HANDLE)test_detour_handle);
+    ret = DetourSetExclusiveACL(new ULONG(), 1, (TRACED_HOOK_HANDLE)sleep_detour_handle);
 
     pthread_t t;
     pthread_create(&t, NULL, TestSleep, NULL);
     pthread_join(t, NULL);
 
+    DetourUninstallHook(test_detour_handle);
+    DetourUninstallHook(sleep_detour_handle);
 
-    DetourUninstallHook(outHandle);
-    DetourUninstallHook(outHandle2);
-
-    delete outHandle;
-    delete outHandle2;
+    delete test_detour_handle;
+    delete sleep_detour_handle;
 
     sleep(1);
 
